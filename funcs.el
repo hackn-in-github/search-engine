771d37ae241b8c75e50d1c9099cf73ffb60c6b3c
hackn-in-github <mr.hack.1984+github@gmail.com>
2018-02-24 14:36:06 +0900

    migemo 対応に修正(helmのみ)

--- funcs.el
+++ funcs.el
@@ -9,21 +9,38 @@
 ;;
 ;;; License: GPLv3
 
-(defun spacemacs//search-engine-source (engines)
-  "return a source for helm selection"
-  `((name . "Search Engines")
-    (candidates . ,(mapcar (lambda (engine)
-                             (cons (plist-get (cdr engine) :name)
-                                   (intern (format "engine/search-%S"
-                                                   (car engine)))))
-                           engines))
-    (action . (lambda (candidate) (call-interactively candidate)))))
+;(defun spacemacs//search-engine-source (engines)
+;  "return a source for helm selection"
+;  `((name . "Search Engines")
+;    (candidates . ,(mapcar (lambda (engine)
+;                             (cons (plist-get (cdr engine) :name)
+;                                   (intern (format "engine/search-%S"
+;                                                   (car engine)))))
+;                           engines))
+;    (action . (lambda (candidate) (call-interactively candidate)))))
+;
+; migemo 対応にする(参考 http://syohex.hatenablog.com/entry/2015/10/10/171926)
+(with-eval-after-load "helm"
+  (defvar spacemacs//search-engine-source-with-migemo
+    (helm-build-sync-source "Search Engines"
+      :candidates (mapcar (lambda (engine)
+                            (cons (plist-get (cdr engine) :name)
+                                  (intern (format "engine/search-%S"
+                                                  (car engine)))))
+                          search-engine-alist)
+      :action (lambda (candidate) (call-interactively candidate))
+      :migemo t)))
 
 (defun spacemacs/helm-search-engine-select ()
   "Set search engine to use with helm."
   (interactive)
-  (helm :sources (list (spacemacs//search-engine-source
-                        search-engine-alist))))
+  (helm :sources `(spacemacs//search-engine-source-with-migemo)))
+
+;(defun spacemacs/helm-search-engine-select ()
+;  "Set search engine to use with helm."
+;  (interactive)
+;  (helm :sources (list (spacemacs//search-engine-source
+;                        search-engine-alist))))
 
 (defun spacemacs/ivy-search-engine-select ()
   "Set search engine to use with ivy."
